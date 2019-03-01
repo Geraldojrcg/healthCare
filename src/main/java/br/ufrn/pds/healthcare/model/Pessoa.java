e76d0b8ae60fe86825f3467d44f83ca3b00b521e
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-03-01 09:12:30 -0300

    Adiciona model de Pessoa

---
+++ src/main/java/br/ufrn/pds/healthcare/model/Pessoa.java
@@ -0,0 +1,78 @@
+package br.ufrn.pds.healthcare.model;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Entity
+public class Pessoa extends EntidadeAbstrata {
+    @CPF
+    @NotEmpty
+    @Length(min = 11, max = 11)
+    private String cpf;
+
+    @NotEmpty
+    private String nome;
+
+    private String sexo;
+    private String telefone;
+
+    @NotEmpty
+    private LocalDate dataDeNascimento;
+
+    @OneToOne
+    private Endereco endereco;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}

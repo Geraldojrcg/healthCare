467fae048073b108e95033f89eeb6cc09ac81abf
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-03-01 08:48:16 -0300

    Adciona model de Endereço

---
+++ src/main/java/br/ufrn/pds/healthcare/model/Endereco.java
@@ -0,0 +1,61 @@
+package br.ufrn.pds.healthcare.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Endereco extends EntidadeAbstrata {
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String cep;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}

5dd0506ba40af1287c8e7f430a07dce04fcc8890
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-03-10 09:26:05 -0300

    Adiciona nome de exibição no status da consulta #16

--- src/main/java/br/ufrn/pds/healthcare/model/ConsultaStatus.java
+++ src/main/java/br/ufrn/pds/healthcare/model/ConsultaStatus.java
@@ -1,5 +1,21 @@
 package br.ufrn.pds.healthcare.model;
 
 public enum ConsultaStatus {
-    REMARCADA, CANCELADA, EM_ATENDIMENTO, ATENDIDA, ATRASADA, MARCADA
+
+    MARCADA("Marcada"),
+    REMARCADA("Remarcada"),
+    CANCELADA("Cancelada"),
+    EM_ATENDIMENTO("Em atendimento"),
+    ATENDIDA("Atendida"),
+    ATRASADA("Atrasada");
+
+    private final String nomeExibicao;
+
+    ConsultaStatus(String nomeExibicao) {
+        this.nomeExibicao = nomeExibicao;
+    }
+
+    public String getNomeExibicao() {
+        return nomeExibicao;
+    }
 }

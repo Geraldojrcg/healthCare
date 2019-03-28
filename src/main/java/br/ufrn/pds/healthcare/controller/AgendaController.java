87373876d2dd319de63482665f432367ba9ead21
Geraldo Fernandes da Silva Junior <geraldojrcg@A309-18L.imd.local>
2019-03-28 16:24:21 -0300

    arrumando bug na agenda

--- src/main/java/br/ufrn/pds/healthcare/controller/AgendaController.java
+++ src/main/java/br/ufrn/pds/healthcare/controller/AgendaController.java
@@ -1,15 +1,28 @@
 package br.ufrn.pds.healthcare.controller;
 
+import br.ufrn.pds.healthcare.service.interfaces.ConsultaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import br.ufrn.pds.healthcare.service.interfaces.ConsultaService;
 
 @Controller
 @RequestMapping("dashboard/agenda")
 public class AgendaController {
-
+	
+	private final ConsultaService consultaService;
+	
+	public AgendaController(ConsultaService consultaService) {
+		// TODO Auto-generated constructor stub
+		 this.consultaService = consultaService;
+	}
+	
     @GetMapping
-    public String agenda() {
+    public String agenda(Model model) {
+    	model.addAttribute("consultasMarcadas", consultaService.buscarMarcadas());
         return "agenda/agenda";
     }
 }
